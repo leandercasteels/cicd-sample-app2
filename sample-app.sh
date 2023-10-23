84ebb148f38e7192b87b08b58b7f7e57d24caf6c
leandercasteels <71777159+leandercasteels@users.noreply.github.com>
2023-10-23 11:33:03 +0200

    Update sample-app.sh

--- sample-app.sh
+++ sample-app.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-#if [[ ! -d "tempdir" ]]; then
-  #mkdir tempdir
-  #mkdir tempdir/templates
-  #mkdir tempdir/static
-#fi
+if [[ ! -d "tempdir" ]]; then
+  mkdir tempdir
+  mkdir tempdir/templates
+  mkdir tempdir/static
+fi
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
